07795dc7ca180e927894552e51ca3f0974046fb8
LHardy7 <lhardy7@aum.edu>
2020-12-04 23:47:03 -0600

    WebsiteAUM

---
+++ AUMWebsite/website.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+
+//Submit button for Teacher Assistant Application onclick
+        $("#submit").click(function() {
+            $("#button").submit(); 
+        });
+    
+});
